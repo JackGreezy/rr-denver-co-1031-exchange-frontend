c1654c702cdc8f88b7836049365ed07ebd119462
Jack Greenberg <jackagreenberg@outlook.com>
2026-08-06 12:43:13 -0700

    Protect contact forms with invisible Turnstile

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
         <Footer />
         <StickyCta />
         <Analytics />
+              <script src="/turnstile-contact.js" defer></script>
       </body>
     </html>
   );
